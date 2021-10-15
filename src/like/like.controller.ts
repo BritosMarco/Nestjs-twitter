d53ac459ef1fbce046042c55cd4f5fcd19441ff9
BritosMarco <85652672+BritosMarco@users.noreply.github.com>
2021-10-14 21:07:08 -0300

    Add files via upload

--- src/like/like.controller.ts
+++ src/like/like.controller.ts
@@ -5,24 +5,40 @@ import {
   Param,
   Delete,
   UseGuards,
+  ValidationPipe,
+  ParseIntPipe,
+  UsePipes,
+  Req,
 } from '@nestjs/common';
 import { Like } from '.prisma/client';
 import { LikeService } from './like.service';
 import { AuthGuard } from '@nestjs/passport';
+import { CreateLikeDto } from './like.dto';
 
 /* A linha baixo Procura quem esta gerenciando o jwt, no nosso caso o nosso strategy.ts*/
-@UseGuards(AuthGuard('jwt'))
+
 @Controller('like')
 export class LikeController {
   constructor(private service: LikeService) {}
 
-  @Post()
-  create(@Body() data: Like): Promise<Like> {
-    return this.service.create(data);
+  @UseGuards(AuthGuard('jwt'))
+  @UsePipes(ValidationPipe)
+  @Post('like')
+  async create(@Body() like: CreateLikeDto, @Req() Req): Promise<Like> {
+    const user = Req.user.id;
+
+    return this.service.create(like, user);
   }
 
+  @UseGuards(AuthGuard('jwt'))
+  @UsePipes(ValidationPipe)
   @Delete('/delete/:id')
-  async delete(@Param('id') id: string) {
-    return this.service.deleteOneLike({ id: Number(id) });
+  async delete(
+    @Param('id', ParseIntPipe) id: number,
+    @Req() Req,
+  ): Promise<Like> {
+    const user = Req.user.id;
+
+    return this.service.deleteOneLike(id, user);
   }
 }
--- src/like/like.service.ts
+++ src/like/like.service.ts
@@ -1,18 +1,38 @@
-import { Injectable } from '@nestjs/common';
-import { Prisma, Like, User } from '@prisma/client';
+import {
+  Injectable,
+  NotFoundException,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { Like } from '@prisma/client';
 import { PrismaService } from '../prisma.service';
+import { CreateLikeDto } from './like.dto';
 
 @Injectable()
 export class LikeService {
   constructor(private db: PrismaService) {}
 
-  async create(data: Prisma.LikeCreateInput): Promise<Like> {
-    const like = await this.db.like.create({ data });
-
-    return like;
+  async create(data: CreateLikeDto, user: number): Promise<Like> {
+    return await this.db.like.create({
+      data: {
+        ...data,
+        userId: user,
+      },
+    });
   }
 
-  async deleteOneLike(where: Prisma.LikeWhereUniqueInput): Promise<Like> {
-    return this.db.like.delete({ where });
+  async deleteOneLike(id: number, user: number): Promise<Like> {
+    const userLike = await this.db.like.findUnique({
+      where: { id },
+      select: {
+        userId: true,
+      },
+    });
+    if (!userLike) {
+      throw new NotFoundException();
+    }
+    if (userLike.userId != user) {
+      throw new UnauthorizedException();
+    }
+    return this.db.like.delete({ where: { id } });
   }
 }
